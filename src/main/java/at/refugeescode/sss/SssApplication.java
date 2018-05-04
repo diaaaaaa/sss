9088a9a93ec04a7d13d8ecfa5b34effac63a0e76
diaaaaaa <ite.diaa@gmail.com>
2018-05-04 14:30:09 +0200

    sss

---
+++ src/main/java/at/refugeescode/sss/SssApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.sss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SssApplication.class, args);
+	}
+}
